74c417031ca7ed54f3164b169c18479df0f8787d
MaztuStudio <121517196+MaztuStudio@users.noreply.github.com>
2023-11-19 12:44:03 -0700

    added !user  functionality

--- resources/views/confirmData.blade.php
+++ resources/views/confirmData.blade.php
@@ -7,13 +7,21 @@
 <div class="bg-image"></div>
 
 <div class="bg-text">
+  @if (is_null($usuario))
+  <h1>Este Usuario No Existe</h1>
+  <hr>
+  <a href="{{route("usuario.index")}}" class="btn btn-danger btn-lg">Volver</a>
+</div>
+  @else
 <h1>¿Esta informacion es correcta?</h1>
 <hr>
-
-<table class="table table-light">
+<div class="table-responsive">
+<table class="table table-light table-sm" style="width=">
   <thead>
     <tr>
       <th>Nombre</th>
+      <th>Apellido P</th>
+      <th>Apellido M</th>
       <th>Numero de Afiliacion</th>
       <th>Telefono</th>
       <th>Consultorio</th>
@@ -22,18 +30,21 @@
   <tbody>
     <tr>
       
-      <form action="{{route('usuario.update',$usuario->id)}}" method="POST">
-        @method('PUT')
+      <form action="{{route('espera.store')}}" method="POST">
         @csrf
-        <td>{{$usuario->nombre}} {{$usuario->paterno}} {{$usuario->materno}}</td>
-        <td>{{$usuario->id}}</td>
-        <td>{{$usuario->telefono}}</td>
-        <td>{{$usuario->consultorio}}</td>
+        <td><input type="text" name="nombre" value="{{$usuario->nombre}}" disabled></td>
+        <td><input type="text" name="paterno" value="{{$usuario->paterno}}" disabled></td>
+        <td><input type="text" name="materno" value="{{$usuario->materno}}" disabled></td>
+        <td><input type="text" name="id" value="{{$usuario->id}}" disabled></td>
+        <td><input type="text" name="telefono" value="{{$usuario->telefono}}" disabled></td>
+        <td><input type="text" name="consultorio" value="{{$usuario->consultorio}}" disabled></td>
     </tr>
   </tbody>
 </table>
+</div>
 <input type="submit" class="btn btn-success btn-lg" value="‎ ‎ ‎ Si ‎ ‎ ‎ ">
 <a href="{{route("usuario.index")}}" class="btn btn-danger btn-lg">Volver</a>
 </form>
 </div>
+@endif
 @endsection
