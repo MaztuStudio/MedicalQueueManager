18d9fbd4a1767a66f79a99cd1064ab56f74201ff
MaztuStudio <121517196+MaztuStudio@users.noreply.github.com>
2023-11-19 18:49:50 -0700

    Added general design for nurse's view

--- resources/views/turnDisplay.blade.php
+++ resources/views/turnDisplay.blade.php
@@ -4,20 +4,55 @@
 <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css')}}">
 
 <ul class="nav nav-tabs" id="myTab" role="tablist">
+
+
+  @for ($i = 1; $i < 4; $i++)
   <li class="nav-item" role="presentation">
-    <button class="nav-link" id="c1-tab" data-bs-toggle="tab" data-bs-target="#c1-tab-pane" type="button" role="tab" aria-controls="c1-tab-pane" aria-selected="true">C 1</button>
-  </li>
-  @for ($i = 2; $i < 12; $i++)
-  <li class="nav-item" role="presentation">
-    <button class="nav-link" id="c{{$i}}-tab" data-bs-toggle="tab" data-bs-target="#c{{$i}}-tab-pane" type="button" role="tab" aria-controls="c{{$i}}-tab-pane" aria-selected="false">C {{$i}}</button>
+    <button class="nav-link" id="c{{$i}}-tab" data-bs-toggle="tab" data-bs-target="#c{{$i}}-tab-pane" type="button" role="tab" aria-controls="c{{$i}}-tab-pane" aria-selected="false">Consultorio {{$i}}</button>
   </li>
   @endfor
+
 </ul>
+
+
 <div class="tab-content" id="myTabContent">
-  <div class="tab-pane fade show active" id="c1-tab-pane" role="tabpanel" aria-labelledby="c1-tab" tabindex="0">
-  </div>
-  @for ($i = 2; $i < 12; $i++)
+  @for ($i = 1; $i < 4; $i++)
   <div class="tab-pane fade" id="c{{$i}}-tab-pane" role="tabpanel" aria-labelledby="c{{$i}}-tab" tabindex="0">
+
+    
+    <div class="container-sm">
+      <div class="table-responsive">
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <td>Turno</td>
+            <td>Nombre</td>
+            <td>Paterno</td>
+            <td>Materno</td>
+            <td>Telefono</td>
+            <td>No Afiliacion</td>
+            <td>Consultorio</td>
+          </tr>
+        </thead>
+        <tbody>
+    @foreach ($datos as $item)
+    @if ($item->consultorio == $i)
+          
+          <tr>
+          <td>{{$item->turno}}</td>
+          <td>{{$item->nombre}}</td>
+          <td>{{$item->paterno}}</td>
+          <td>{{$item->materno}}</td>
+          <td>{{$item->telefono}}</td>
+          <td>{{$item->afiliacion}}</td>
+          <td>{{$item->consultorio}}</td>
+        </tr>
+    @endif
+    @endforeach
+</tbody>
+</table>
+</div>
+</div>
   </div>
   @endfor
 </div>
