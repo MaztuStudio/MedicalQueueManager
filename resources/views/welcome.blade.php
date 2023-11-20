19591500b20870a13cb872db72b67005ebe1a8ea
MaztuStudio <121517196+MaztuStudio@users.noreply.github.com>
2023-11-19 18:50:16 -0700

    Added confirmation alert for succesful op

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,13 @@
 <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css')}}">
 <div class="bg-image"></div>
 
+
 <div class="bg-text">
+  @if ($mensaje = Session::get('success'))
+<div class="alert alert-success" role="alert">
+    {{$mensaje}}
+</div>
+@endif
     <h1 style="font-size:50px">Tomar Turno</h1>
   <p>
     <form action="{{route('usuario.edit')}}" method="GET">
@@ -14,9 +20,10 @@
         <br>
         <input type="number" name="nAfiliacion" required>
         <br>
-  <input type="submit" class="btn btn-success btn-lg" value="Continuar">
-  
+        <input type="submit" class="btn btn-success btn-lg" value="Continuar">
     </form>
+    <br>
+    <a href="{{route("espera.list")}}" class="btn btn-outline-light">Ver Lista</a>
   </p>
 </div>
 
